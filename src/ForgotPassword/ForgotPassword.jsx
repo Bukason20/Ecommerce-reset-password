746eb9776123d7832a61e8536751c38b86f6741d
Bukason20 <chukwuebukaedward20@gmail.com>
2023-07-05 01:32:41 +0100

    modified files

--- src/ForgotPassword/ForgotPassword.jsx
+++ src/ForgotPassword/ForgotPassword.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "./forgotPassword.module.css"
 import Logo from "../images/logo.png"
+import Check from "../images/check.png"
+import Close from "../images/close.png"
 
 
 function ForgotPassword() {
     const [email, setEmail] = useState("")
     const [loading, setLoading] = useState(false)
-    const [showModal, setShowModal] = useState("")
+    const [showModal, setShowModal] = useState(false)
+    const [data, setData] = useState("")
+    const [message, setMessage] = useState("")
 
     const forgotPassword = (e) => {
         e.preventDefault()
@@ -25,13 +29,26 @@ function ForgotPassword() {
         .then((res) => res.json())
         .then((data) =>{
           setLoading(false)
+          data.success ? setShowModal(true) : setShowModal(false)
+          setData(data)
           console.log(data)
         })
         .catch((err) => console.log(err))
     }
+
+    useEffect(() => {
+      if(data !== undefined && data !== null){
+        if(data.success !== null && data.success !== undefined && data.success == false){
+          setMessage(data.message)
+          setTimeout(() => {
+            setMessage("")
+          }, 2000)
+        }
+      }
+    }, [data])
   return (
     <div className = {styles.Container}>
-      <div className= {styles.overlay}></div>
+      <div className= {`${styles.overlay} ${!showModal ? styles.hidden : ""}`}></div>
       <div className= {styles.logo}>
         <img src= {Logo} alt=""/>
       </div>
@@ -39,9 +56,11 @@ function ForgotPassword() {
             <h2>Forgot password</h2>
             <p>Input your email address to reset password</p>
 
+            <p className= {styles.error}>{message}</p>
+
             <div className= {styles.inputGrp}>
               <label>Email Address</label>
-              <input type="email" name="" id="" placeholder="Email Address" value={email} onChange={(e) => setEmail(e.target.value)} />
+              <input type="email" name="" id="" placeholder="Email Address" value={email} onChange={(e) => setEmail(e.target.value)} required/>
             </div>
             
             <button type='submit' className = {styles.btn}>
@@ -53,6 +72,17 @@ function ForgotPassword() {
               <p>For further support, kindly visit our help center or contact our customer service team.</p>
             </div>
         </form>
+
+      {showModal && <div className= {styles.modal}>
+        <img src= {Check} alt="" id = {styles.check}/>
+        <h2>Password Reset Link Sent</h2>
+
+        <p>Kindly proceed to your email and click on the provided link to reset your Bale password</p>
+
+        <div className= {styles.close} onClick = {() => setShowModal(false)}>
+          <img src= {Close} alt=""/>
+        </div>
+      </div> }
         
     </div>
   )
--- src/ResetPassword/ResetPassword.jsx
+++ src/ResetPassword/ResetPassword.jsx
@@ -1,24 +1,85 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import {useParams} from 'react-router-dom'
+import styles from "./resetPassword.module.css"
+import Logo from "../images/logo.png"
+import Eye from "../images/eye.png"
 
 function ResetPassword() {
     const [newPassword, setNewPassword] = useState("")
+    const [loading, setLoading] = useState(false)
+    const [passwordVisibility, setPasswordVisibility] = useState(false)
+    const [data, setData] = useState("")
+    const [message, setMessage] = useState("")
 
     const {token} = useParams()
     console.log(token)
 
     const forgotPassword = (e) => {
         e.preventDefault();
+        setLoading(true)
+        const formData = {
+          password: newPassword
+        }
+
+        const response = fetch(`https://balethriftstore.onrender.com/api/v1/reset/password/${token}`, {
+          method: "POST",
+          body: JSON.stringify(formData),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        .then((res) => res.json())
+        .then((data) =>{
+          setLoading(false)
+          setData(data)
+          console.log(data)
+        })
+        .catch((err) => console.log(err))
 
     }
+    useEffect(() => {
+      if(data !== undefined && data !== null){
+        if(data.success !== null && data.success !== undefined && data.success == true){
+          setMessage(data.message)
+          setTimeout(() => {
+            setMessage("")
+          }, 2000)
+        }else if(data.success !== null && data.success !== undefined && data.success == false){
+          setMessage(data.message.message)
+          setTimeout(() => {
+            setMessage("")
+          }, 2000)
+        }
+      }
+    }, [data])
   return (
-    <div>
+    <div className = {styles.Container}>
+      <div className= {styles.logo}>
+        <img src= {Logo} alt=""/>
+      </div>
         <form onSubmit={forgotPassword}>
             <h2>Reset password</h2>
+            <p>Input your new password</p>
 
-            <input type="password" name="" id="" placeholder="Email Address" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
-            <button type='submit'>Reset password</button>
+            <p className= {`${data !== undefined && data !== null && data.success == true ? styles.success : styles.error}`}>{message}</p>
+
+            <div className= {styles.inputGrp}>
+              <label>Password</label>
+              <div className= {styles.password}>
+                <input type= {`${passwordVisibility ? "text" : "password"}`} name="" id="" placeholder="********" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} required/>
+                <img src= {Eye} alt="" onClick={() => setPasswordVisibility(!passwordVisibility)} id = {styles.seePassword}/>
+              </div>
+              
+            </div>
+            <button type='submit' className = {styles.btn}>
+              {loading && "Loading..."}
+              {!loading && "Reset Password"}
+            
+            </button>
+            <div className= {styles.enquiries}>
+              <p>For further support, kindly visit our help center or contact our customer service team.</p>
+            </div>
 
         </form>
     </div>
